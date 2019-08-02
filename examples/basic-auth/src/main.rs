7423d170018fa616019b93d086a932053a2080c7
PrivateRookie <996514515@qq.com>
2019-08-02 22:53:48 +0800

    fix example

--- examples/basic-auth/src/main.rs
+++ examples/basic-auth/src/main.rs
@@ -2,6 +2,7 @@ use gotham::pipeline::new_pipeline;
 use gotham::pipeline::single::single_pipeline;
 use gotham::router::builder::*;
 use gotham::router::Router;
+use gotham::state::State;
 use gotham_middleware_basicauth::AuthMiddleWare;
 
 fn router() -> Router {
@@ -12,8 +13,12 @@ fn router() -> Router {
     );
 
     build_router(chain, pipeline, |route| {
-        route.get("/").to_file("src/static/index.html");
+        route.get("/").to(say_hello);
     })
+}
+
+fn say_hello(state: State) -> (State, &'static str) {
+    (state, "Hello Auththorized User!")
 }
 
 fn main() {
